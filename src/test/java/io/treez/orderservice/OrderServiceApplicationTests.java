91561f26c5826091261f1ad02a503e0bc7bf2e94
Adithya Nayabu <adithya.nayabu@gmail.com>
2020-02-04 14:35:19 -0500

    Initial Setup

---
+++ src/test/java/io/treez/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.treez.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
